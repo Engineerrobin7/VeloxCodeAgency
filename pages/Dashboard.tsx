f348094ef9208d4eb7ac868426b9a005907bc85f
Robin Singh <engineerrobin905@gmail.com>
2026-01-21 20:29:13 +0530

    vercel fixes

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { SEO } from '../components/SEO';
-import { getData, addData } from '../api';
+import { getData, addData } from '../src/api';
 
 const stats = [
   { label: 'Active Projects', value: '2' },
